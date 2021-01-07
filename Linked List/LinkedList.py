0de660b0c1563fb1bd375771cf0ddef2440a21f4
fk798 <fk798@nyu.edu>
2021-01-07 03:15:08 -0500

    added a buggy isPalindrome

--- Linked List/LinkedList.py
+++ Linked List/LinkedList.py
@@ -5,7 +5,8 @@ import random
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
-    
+        self.length = 0
+
     def append(self, data):
         if type(data) == Node:
             node = data
@@ -13,14 +14,17 @@ class SinglyLinkedList:
             node = Node(data)
         if self.head == None:
             self.head = node
+            self.length += 1
             return
         trav = self.head
         while trav.next != None:
             trav = trav.next
         trav.next = node
+        self.length += 1
 
     def delete(self, data): # really redundant, could divide into two different functions for node and data value
         if self.head == None:
+            self.length = 0
             return
         if type(data) == Node: # deleting a node
             if self.head == data:
@@ -50,6 +54,7 @@ class SinglyLinkedList:
                         temp.disconnect()
                         break
                     trav = trav.next
+        self.length -= 1
 
     def get(self, n: int):
         trav = self.head
@@ -234,7 +239,7 @@ def medianOfList(linkedlist): # assumes no cycle
 #print(medianOfList(alist).data)
 
 
-def reverse(linkedlist):
+def reverse(linkedlist): # reverse's whole linked list - didn't think about nodes at first whoops
     head = linkedlist.head
     if head == None or head.next == None:
         return head
@@ -248,6 +253,60 @@ def reverse(linkedlist):
     
     linkedlist.head = prev
 
-print(alist)
-reverse(alist)
-print(alist)
+#print(alist)
+#reverse(alist)
+#print(alist)
+
+# reverse part of linked list - need reference to the node
+def reverseNode(head):
+    if head == None or head.next == None:
+        return head
+    prev = None
+    curr = head
+    while curr != None:
+        nex = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nex
+
+
+def stringLL(string):
+    linkedlist = SinglyLinkedList()
+    for elem in string:
+        linkedlist.append(elem.upper())
+    return linkedlist
+
+# works but doesn't set the list back correctly - will fix later
+def isPalindrome(linkedlist): # O(N) time O(1) space
+    head = linkedlist.head
+    length = linkedlist.length
+    trav = head 
+    for i in range(int(length/2)):
+        trav = trav.next
+    # at middle of list - reverse this part
+    # first get the last node
+    head2 = trav
+    while head2.next != None:
+        head2 = head2.next
+    # now reverse
+    reverseNode(trav)
+    reverseHead2 = head2
+    while head != None and head2 != None:
+        if head.data != head2.data:
+            return False
+        head = head.next
+        head2 = head2.next
+    printhead2 = reverseHead2
+    while printhead2:
+        print(printhead2.data)
+        printhead2 = printhead2.next
+    reverseNode(reverseHead2)
+    return True
+
+word = "moomo"
+headWord = stringLL(word)
+print(headWord)
+print(headWord.length)
+
+print(isPalindrome(headWord))
+print(headWord)
